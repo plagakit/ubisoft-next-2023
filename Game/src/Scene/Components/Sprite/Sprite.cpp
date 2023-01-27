925d603896e5bb6bf69daa1f3a794b40d5615312
wer8888 <91093536+wer8888@users.noreply.github.com>
2023-01-27 16:36:15 -0500

    Add sprite

---
+++ Game/src/Scene/Components/Sprite/Sprite.cpp
@@ -0,0 +1,6 @@
+#include <stdafx.h>
+
+#include "Sprite.h"
+
+Sprite::Sprite() : width(0.0f), height(0.0f), r(255), g(255), b(255) {}
+
---
+++ Game/src/Scene/Components/Sprite/Sprite.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdafx.h>
+
+
+struct Sprite {
+
+	float width, height;
+	int r, g, b;
+
+};
