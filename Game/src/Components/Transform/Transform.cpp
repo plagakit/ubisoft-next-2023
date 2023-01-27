27dfd3e2f41ae2a1d4fdca6eec35aecd153ca1bf
wer8888 <91093536+wer8888@users.noreply.github.com>
2023-01-27 14:44:53 -0500

    Add Transform component

---
+++ Game/src/Components/Transform/Transform.cpp
@@ -0,0 +1,5 @@
+#include "Transform.h"
+
+Transform::Transform() : 
+	position(Vector2()), velocity(Vector2()), scale(Vector2(1.0f, 1.0f)), rotation(0.0f)
+{}
---
+++ Game/src/Components/Transform/Transform.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Vector2/Vector2.h>
+
+struct Transform {
+
+	Vector2 position, velocity, scale;
+	float rotation;
+
+	Transform();
+
+};
