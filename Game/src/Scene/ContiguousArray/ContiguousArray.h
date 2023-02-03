08431cd6163cda58732ef0e4103e7f41fdcb7cab
wer8888 <91093536+wer8888@users.noreply.github.com>
2023-02-03 17:02:20 -0500

    Add barebones ContiguousArray

---
+++ Game/src/Scene/ContiguousArray/ContiguousArray.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <stdafx.h>
+
+#include "IContiguousArray.h"
+#include <Scene/Scene.h>
+#include <Scene/Components/Sprite/Sprite.h>
+#include <Scene/Components/Transform/Transform.h>
+
+
+template<typename T>
+class ContiguousArray : public IContiguousArray {
+
+public:
+	ContiguousArray();
+
+	T& Get(Entity id);
+	//int GetSize() const;
+
+	void Add(Entity id, T component);
+	void Remove(Entity id);
+
+	
+private:
+	std::vector<T> m_items;
+	std::unordered_map<Entity, int> m_entityMap;
+	int m_numItems;
+
+};
+
+
+template<typename T>
+ContiguousArray<T>::ContiguousArray()
+	: m_items(), m_entityMap(), m_numItems(0)
+{}
+
+template<typename T>
+T& ContiguousArray<T>::Get(Entity id)
+{
+	// TODO: add assert
+	auto itemIndex = m_entityMap.find(id);
+	return m_items[itemIndex->first];
+}
+
+template<typename T>
+void ContiguousArray<T>::Add(Entity id, T item)
+{
+	// TODO: add assert
+	m_items.push_back(item);
+	m_entityMap[id] = m_numItems;
+	m_numItems++;
+}
+
+template<typename T>
+void ContiguousArray<T>::Remove(Entity id)
+{
+	// TODO: add assert
+
+	// Swap memory of deleted component and last component in vector
+	int removed_index = m_entityMap[id];
+	m_items[removed_index] = m_components[m_numItems];
+
+	m_items.erase(m_items.begin() + m_numItems);
+	m_entityMap.erase(m_numItems);
+
+	m_numItems--;
+}
+
+/*
+template <typename T>
+int ComponentArray<T>::GetSize() const
+{
+	return m_activeComponents;
+}
+*/
+
+template class ContiguousArray<Transform>;
+template class ContiguousArray<Sprite>;
---
+++ Game/src/Scene/ContiguousArray/IContiguousArray.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdafx.h>
+
+// class that exists only for polymorphism w/ vector of different types of component arrays
+class IContiguousArray {};
