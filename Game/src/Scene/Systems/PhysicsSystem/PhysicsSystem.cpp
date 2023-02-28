0e3d23f611793ccb43348e6c13056fd18fdc3ac1
wer8888 <91093536+wer8888@users.noreply.github.com>
2023-02-28 00:45:29 -0500

    Add not made yet physics system functions

--- Game/src/Scene/Systems/PhysicsSystem/PhysicsSystem.cpp
+++ Game/src/Scene/Systems/PhysicsSystem/PhysicsSystem.cpp
@@ -20,6 +20,55 @@ void PhysicsSystem::UpdatePosition(Scene& scene)
 		{
 			tf.position += ph.velocity * scene.m_deltaTime;
 			tf.rotation += ph.angularVelocity * scene.m_deltaTime;
+
+			if (abs(ph.velocity.x) > 0 || abs(ph.velocity.y) > 0)
+			{
+				if (scene.HasComponent<CircleBounds>(id))
+					UpdateCollision(scene, id, scene.GetComponent<CircleBounds>(id));
+
+				if (scene.HasComponent<BoxBounds>(id))
+					UpdateCollision(scene, id, scene.GetComponent<BoxBounds>(id));
+			}
 		}
 	}
+}
+
+void PhysicsSystem::UpdateCollision(Scene& scene, Entity id, BoxBounds& bb)
+{
+	for (Entity other : scene.GetEntities<Transform, Physics>())
+	{
+		if (other == id) continue;
+
+
+
+	}
+}
+
+void PhysicsSystem::UpdateCollision(Scene& scene, Entity id, CircleBounds& cb)
+{
+	for (Entity other : scene.GetEntities<Transform, Physics>())
+	{
+		if (other == id) continue;
+
+
+
+	}
+}
+
+// Circle & circle
+bool PhysicsSystem::IsColliding(Scene& scene, Entity id1, Entity id2, CircleBounds& cb1, CircleBounds& cb2)
+{
+
+}
+
+// Box & box
+bool PhysicsSystem::IsColliding(Scene& scene, Entity id1, Entity id2, BoxBounds& bb1, BoxBounds& bb2)
+{
+
+}
+
+// Circle & box
+bool PhysicsSystem::IsColliding(Scene& scene, Entity id1, Entity id2, CircleBounds& cb, BoxBounds& bb)
+{
+
 }
--- Game/src/Scene/Systems/PhysicsSystem/PhysicsSystem.h
+++ Game/src/Scene/Systems/PhysicsSystem/PhysicsSystem.h
@@ -2,11 +2,25 @@
 
 #include <stdafx.h>
 
+#include "Core/Signal/Signal.h"
+
 class Scene;
+struct BoxBounds;
+struct CircleBounds;
 
 class PhysicsSystem {
 
 public:
+	Signal<Entity, Entity> s_onCollision;
+	Signal<Entity, Entity> s_onTrigger;
 	void UpdatePosition(Scene& scene);
+
+private:
+	void UpdateCollision(Scene& scene, Entity id, BoxBounds& bb);
+	void UpdateCollision(Scene& scene, Entity id, CircleBounds& cb);
+
+	bool IsColliding(Scene& scene, Entity id1, Entity id2, CircleBounds& cb1, CircleBounds& cb2);
+	bool IsColliding(Scene& scene, Entity id1, Entity id2, BoxBounds& bb1, BoxBounds& bb2);
+	bool IsColliding(Scene& scene, Entity id1, Entity id2, CircleBounds& cb, BoxBounds& bb);
 
 };
