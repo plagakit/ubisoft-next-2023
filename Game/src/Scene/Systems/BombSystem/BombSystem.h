7378023fac9477a45ec8d8017435bc39b7429439
wer8888 <91093536+wer8888@users.noreply.github.com>
2023-03-04 16:43:11 -0500

    Add comment on hardcoding particle data

--- Game/src/Scene/Systems/BombSystem/BombSystem.h
+++ Game/src/Scene/Systems/BombSystem/BombSystem.h
@@ -17,6 +17,13 @@ class BombSystem {
 	const int DEFAULT_DAMAGE = 1;
 	const float DEFAULT_KNOCKBACK = 0.0f;
 
+	// Defining the particle emission info feels wrong - what if I want it to change
+	// during the game, or be controlled by something else? If I had more time, I
+	// would have create a ParticleEmitter component to hold all the data like this
+	// and something like a CreateParticle() function in ParticleSystem.cpp that uses
+	// said ParticleEmitter, but I didn't have enough time. Hardcoding values like this
+	// works for this game and its small scope, but had I more time I would account for
+	// expansion.
 	const int BOMB_PARTICLE_COUNT = 50;
 	const std::pair<float, float> BOMB_PARTICLE_SPEED = { 50.0f, 150.0f };
 	const std::pair<float, float> BOMB_PARTICLE_ROTATION_SPEED = { -4.0f, 4.0f };
